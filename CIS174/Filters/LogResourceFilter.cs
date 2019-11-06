16336ea10e8056519aa42bb7ae1dbf7fd035415c
Charwillbro <35745538+Charwillbro@users.noreply.github.com>
2019-11-06 16:02:21 -0600

    Initial Commit for assignment 11.1

---
+++ CIS174/Filters/LogResourceFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174.Filters
+{
+    public class LogResourceFilter : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            Debug.WriteLine("Charles - LogResource Filter Executed");
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            Debug.WriteLine("Charles - LogResource Filter Executing");
+        }
+    }
+
+}
