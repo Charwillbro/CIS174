e0542366ccb5f33c6da3907726bfcfd1175d92b6
Charwillbro <35745538+Charwillbro@users.noreply.github.com>
2019-11-15 19:59:03 -0600

    Added authorization on assignment 12.2, did not make valatorre an admin.

---
+++ CIS174/Controllers/Assignment12_2Controller.cs
@@ -0,0 +1,143 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using CIS174.Entities;
+using CIS174.Filters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CIS174.Controllers
+{
+    [Authorize]
+    public class Assignment12_2Controller : Controller
+    {
+        private readonly PersonAccomplishmentContext _context;
+
+        public Assignment12_2Controller(PersonAccomplishmentContext context)
+        {
+            _context = context;
+        }
+
+ 
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.People.ToListAsync());
+        }
+
+      
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People.SingleOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateModelAttribute]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Birthdate,City,State")] Person person)
+        {
+         
+            _context.Add(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+           
+        }
+
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+    
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Birthdate,City,State")] Person person)
+        {
+            if (id != person.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+      
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var person = await _context.People.FindAsync(id);
+            _context.People.Remove(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _context.People.Any(e => e.Id == id);
+        }
+    }
+}
